2da0dced6d5dabd77fc79a56358fce80ce4fc6fc
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-08 06:38:18 +0700

    #21 - Add more data in data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,19 @@
 INSERT INTO product (name, price, product_id, version)
-VALUES ('Mie Goreng Indomie', '2500', '01', '1');
-INSERT INTO product (name, price, product_id, version)
-VALUES ('Mie Indomie Soto', '2000', '01', '1');
-INSERT INTO product (name, price, product_id, version)
-VALUES ('Aqua Gelas', '1000', '01', '1');
+VALUES ('Mie Goreng Indomie', '2500', '01', '1'),
+       ('Mie Indomie Soto', '2000', '01', '1'),
+       ('Milo', '2500', '01', '1'),
+       ('Yakult', '2500', '01', '1'),
+       ('Chimori', '5000', '01', '1'),
+       ('Sosis So Nice', '2500', '01', '1'),
+       ('Nutri Sari', '2500', '01', '1'),
+       ('Marimas', '1500', '01', '1'),
+       ('Pulpen PILOT', '2500', '02', '1'),
+       ('Pulpen BOXY', '7500', '02', '1'),
+       ('Faber Castle', '17500', '02', '1'),
+       ('Pamupus', '2500', '02', '1'),
+       ('Head n Shoulder', '500', '03', '1'),
+       ('Pantene', '500', '03', '1'),
+       ('Clear', '500', '03', '1'),
+       ('Rejoice', '500', '03', '1'),
+       ('Sun Silk', '500', '03', '1'),
+       ('Aqua Gelas', '1000', '01', '1');
