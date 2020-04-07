ce1064e072dd01da05d4eb283c606a30e31fd2b8
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-08 05:54:37 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootdatatable2/SpringBootDatatable2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdatatable2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatatable2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDatatable2Application.class, args);
+    }
+
+}
