f705f9e8bf31d3aca6dd6c5018a71ec1bf8c9bfa
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-08 05:59:00 +0700

    #5 - Add Product Entity

---
+++ src/main/java/com/hendisantika/springbootdatatable2/entity/Product.java
@@ -0,0 +1,55 @@
+package com.hendisantika.springbootdatatable2.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-datatable2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/04/20
+ * Time: 05.58
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "price", length = 19)
+    private BigDecimal price;
+
+    @Column(name = "product_id")
+    private String productId;
+
+    @Version
+    private Integer version;
+
+    public Product(String name, BigDecimal price, String productId, Integer version) {
+        super();
+        this.name = name;
+        this.price = price;
+        this.productId = productId;
+        this.version = version;
+    }
+
+}
