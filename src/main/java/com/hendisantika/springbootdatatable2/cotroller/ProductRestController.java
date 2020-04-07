13ed8b1d0f7e322a677f6d635d9935cec7969084
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-08 06:03:42 +0700

    #10 - Add getProductById in ProductRestController

--- src/main/java/com/hendisantika/springbootdatatable2/cotroller/ProductRestController.java
+++ src/main/java/com/hendisantika/springbootdatatable2/cotroller/ProductRestController.java
@@ -4,6 +4,7 @@ import com.hendisantika.springbootdatatable2.entity.Product;
 import com.hendisantika.springbootdatatable2.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,5 +25,10 @@ public class ProductRestController {
     @GetMapping(path = "/api/products")
     public Iterable<Product> getAllProducts() {
         return productService.getAllProducts();
+    }
+
+    @GetMapping(path = "/api/products/{id}")
+    public Product getProductById(@PathVariable("id") Integer id) {
+        return productService.getProductById(id);
     }
 }
