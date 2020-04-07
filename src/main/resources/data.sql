59a6eead1f801b88179a80ae4afc7ad09d9c2424
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-08 05:57:27 +0700

    #4 - Add sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (name, price, product_id, version)
+VALUES ('Mie Goreng Indomie', '2500', '01', '1');
+INSERT INTO product (name, price, product_id, version)
+VALUES ('Mie Indomie Soto', '2000', '01', '1');
+INSERT INTO product (name, price, product_id, version)
+VALUES ('Aqua Gelas', '1000', '01', '1');
