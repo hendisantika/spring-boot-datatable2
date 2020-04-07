923f5721fae10d5ebf06e9175fe9f984b6105a6b
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-08 06:05:43 +0700

    #15 - Add edit in ProductController

--- src/main/java/com/hendisantika/springbootdatatable2/cotroller/ProductController.java
+++ src/main/java/com/hendisantika/springbootdatatable2/cotroller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
@@ -43,5 +44,11 @@ public class ProductController {
     public String saveProduct(Product product) {
         productService.saveProduct(product);
         return "redirect:/products/" + product.getId();
+    }
+
+    @GetMapping("product/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "productform";
     }
 }
