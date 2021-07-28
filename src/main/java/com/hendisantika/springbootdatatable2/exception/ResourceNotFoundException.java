2a99fc66250ff9e18b1013105a9732eb00edb74c
Hendi Santika <hendisantika@gmail.com>
2021-07-29 04:59:05 +0700

    #56 - Add ResourceNotFoundException

---
+++ src/main/java/com/hendisantika/springbootdatatable2/exception/ResourceNotFoundException.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springbootdatatable2.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-datatable2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/07/21
+ * Time: 12.37
+ */
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException() {
+        super();
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+
+}
